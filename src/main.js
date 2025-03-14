68750a249684b8d51fe4cf842e07c1eb7b25018b
abusizhishen <abusizhishen@gmail.com>
2025-03-15 02:33:40 +0800

    optermize build size

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import 'ant-design-vue/dist/reset.css';
-import Antd from 'ant-design-vue';
+// import Antd from 'ant-design-vue';
 
-createApp(App).use(Antd).mount('#app')
+createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+//自动导入ui-组件 比如说ant-design-vue  element-plus等
+import Components from "unplugin-vue-components/vite";
+//ant-design-vue
+import {AntDesignVueResolver} from "unplugin-vue-components/resolvers";
+import Vue from '@vitejs/plugin-vue'
+import {defineConfig} from "vite";
+import viteCompression from 'vite-plugin-compression';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [
+        Vue(),
+        Components({
+            //ant-design-vue   importStyle = false 样式就没了
+            resolvers: [
+                AntDesignVueResolver({importStyle: "less", resolveIcons: true}),
+            ],
+        }),
+        viteCompression({
+            algorithm: 'brotliCompress', // 或 'gzip'
+            threshold: 20480 // 对大于 10KB 的文件压缩
+        })
+    ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: (id) => {
+                    if (id.includes('node_modules')) {
+                        // 进一步细分 vendor
+                        if (id.includes('ant-design-vue')) return 'antd';
+                        if (id.includes('lodash')) return 'lodash';
+                        return 'vendor';
+                    }
+                },
+            },
+        },
+    },
 })
